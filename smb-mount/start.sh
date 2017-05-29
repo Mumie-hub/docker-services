b7ce02f99b529b5e6966345d8eb4ef0da2fe4c9d
Mumie-hub <cool-mumie@web.de>
2017-05-29 15:56:36 +0200

    bug fixes with mount loop

--- smb-mount/start.sh
+++ smb-mount/start.sh
@@ -6,25 +6,23 @@ echo "Mounting SMB from $SERVERPATH to $MOUNTPOINT at $(date +%Y.%m.%d-%T)"
 #export EnvVariable
 
 function term_handler {
-  kill -SIGTERM ${!} #kill last spawned background process
-  echo "sending SIGTERM to child pid"
   unmount
   echo "exiting now"
-  exit $?
+  exit 0
 }
 
 function unmount {
-  echo "Unmounting: fusermount -u $MOUNTPOINT $(date +%Y.%m.%d-%T)"
+  echo "Unmounting: $MOUNTPOINT $(date +%Y.%m.%d-%T)"
   umount $MOUNTPOINT
 }
 
 trap term_handler SIGHUP SIGINT SIGTERM
 
+mount.cifs -o username=$USERNAME,password=$PASSWORD $SERVERPATH $MOUNTPOINT $MountCommands
+
 while true
 do
-  mount.cifs -o username=$USERNAME,password=$PASSWORD $SERVERPATH $MOUNTPOINT $MountCommands & wait ${!}
-  echo "smb mount crashed"
-  unmount
+sleep 10
 done
 
 exit 144
