d6287755f9a501f73baa36d8cdebe5d69c39c84b
Mumie-hub <cool-mumie@web.de>
2017-05-30 03:55:06 +0200

    ENV of mountoptions changed

--- smb-mount/start.sh
+++ smb-mount/start.sh
@@ -6,19 +6,19 @@ echo "Mounting SMB from $SERVERPATH to $MOUNTPOINT at $(date +%Y.%m.%d-%T)"
 #export EnvVariable
 
 function term_handler {
-  unmount
+  unmount_smb
   echo "exiting now"
   exit 0
 }
 
-function unmount {
+function unmount_smb {
   echo "Unmounting: $MOUNTPOINT $(date +%Y.%m.%d-%T)"
   umount $MOUNTPOINT
 }
 
 trap term_handler SIGHUP SIGINT SIGTERM
 
-mount.cifs -o username=$USERNAME,password=$PASSWORD $SERVERPATH $MOUNTPOINT $MountCommands
+mount.cifs -o $MOUNTOPTIONS $SERVERPATH $MOUNTPOINT
 
 while true
 do
