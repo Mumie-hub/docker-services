bcce6bf787c2fe4234c4561dd0143329d8fa4fab
Mumie-hub <cool-mumie@web.de>
2017-06-02 19:13:47 +0200

    umount with verbose option enabled

--- smb-mount/start.sh
+++ smb-mount/start.sh
@@ -8,12 +8,13 @@ echo "Mounting SMB from $SERVERPATH to $MOUNTPOINT at $(date +%Y.%m.%d-%T)"
 function term_handler {
   unmount_smb
   echo "exiting now"
+  wait 10
   exit 0
 }
 
 function unmount_smb {
   echo "Unmounting: $MOUNTPOINT $(date +%Y.%m.%d-%T)"
-  umount -f $MOUNTPOINT
+  umount -f -v $MOUNTPOINT
 }
 
 trap term_handler SIGHUP SIGINT SIGTERM
