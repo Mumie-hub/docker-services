79d8c51736807d8e99d00904ee4e19d91b5f0a93
vistalba <12324196+vistalba@users.noreply.github.com>
2019-01-06 16:11:50 +0100

    Delete get-version.py

--- teamspeakserver/get-version.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import json
-from urllib.request import urlopen
-
-url = "https://www.server-residenz.com/tools/ts3versions.json"
-response = urlopen(url)
-data = json.loads(response.read().decode("utf-8"))
-print(data['latest'])
