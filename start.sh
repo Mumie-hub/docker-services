010739bb7bdc44e621c563ae5f7d0bd9a7a72575
vistalba <12324196+vistalba@users.noreply.github.com>
2019-05-15 16:32:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,7 +34,7 @@ trap term_handler SIGINT SIGTERM
 trap cache_handler SIGHUP
 
 #mount rclone remote and wait
-/usr/sbin/rclone --config $ConfigPath mount $RemotePath $MountPoint $MountCommands &
+/usr/sbin/rclone --config $ConfigPath --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36' mount $RemotePath $MountPoint $MountCommands &
 wait ${!}
 echo "rclone crashed at: $(date +%Y.%m.%d-%T)"
 fuse_unmount
