25771f47790b0f07eef8b9088b7e46ea422d1b6d
skippy <skippy@honolulu-build.hawaiian.islands>
2019-10-13 01:05:20 -0400

    Changed default route lookup commands

--- sabnzbdvpn/openvpn/start.sh
+++ sabnzbdvpn/openvpn/start.sh
@@ -16,7 +16,7 @@
 
 #set routing gateway for the container
 if [ -n "${LOCAL_NETWORK-}" ]; then
-  eval $(/sbin/ip r l m 0.0.0.0 | awk '{if($5!="tun0"){print "GW="$3"\nINT="$5; exit}}')
+  eval $(/sbin/ip r s 0.0.0.0/0 | awk '{if($5!="tun0"){print "GW="$3"\nINT="$5; exit}}')
   if [ -n "${GW-}" -a -n "${INT-}" ]; then
     echo "adding route to local network $LOCAL_NETWORK via $GW dev $INT"
     /sbin/ip r a "$LOCAL_NETWORK" via "$GW" dev "$INT"
